9a3c23cba84341bd684f6ea247c5b65f8b06d3a7
Ariunbayar B <1480970+ariunbayar@users.noreply.github.com>
2022-01-11 23:09:48 +0800

    Test workflow run on merge

--- notify_schema_changes_to_slack.js
+++ notify_schema_changes_to_slack.js
@@ -84,7 +84,7 @@ async function notifySlack(webhook, pull_request, changes) {
 
     const payload = JSON.stringify({
         username: 'Marketplace graphql BOT',
-        text: `Schema update on <${pull_request.url}|\`${pull_request.title} #${pull_request.title}\`>`,
+        text: `Schema update on <${pull_request.url}|\`${pull_request.title} #${pull_request.number}\`>`,
         attachments: createAttachments(changes),
     })
 
